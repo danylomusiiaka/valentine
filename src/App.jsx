8306a0a2db7449391b72b3d85703cd46f1adb2ec
Danylo Musiiaka <dollarone98@gmail.com>
2024-07-13 18:10:29 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 
-// Spinner component
 const Spinner = ({ text }) => (
   <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-900 bg-opacity-50 text-white z-50">
     <div className="text-center">
@@ -13,15 +12,15 @@ const Spinner = ({ text }) => (
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;  
+
+  const [loading, setLoading] = useState(true);
   const [images, setImages] = useState({
     start: "start.gif",
     no: "no.gif",
     yes: "yes.gif"
   });
-  const [loading, setLoading] = useState(true);
-  const yesButtonSize = noCount * 20 + 16;
 
-  // Preload images
   useEffect(() => {
     const preloadImages = () => {
       const imageKeys = Object.keys(images);
@@ -38,12 +37,16 @@ export default function Page() {
         };
       });
     };
-
     preloadImages();
   }, [images]);
 
+  if (loading) {
+    return <Spinner text="Загружаю твою валентинку 🎀" />;
+  }
+
+
   const handleNoClick = () => {
-    if (noCount < 9) {
+    if (noCount < phrases.length) {
       setNoCount(noCount + 1);
     }
   };
@@ -63,10 +66,6 @@ export default function Page() {
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
-
-  if (loading) {
-    return <Spinner text="Загружаю твою валентинку 🎀" />;
-  }
 
   return (
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
@@ -91,7 +90,7 @@ export default function Page() {
             >
               Так
             </button>
-            {noCount < 9 ? (
+            {noCount < phrases.length ? (
               <button
                 onClick={handleNoClick}
                 className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
